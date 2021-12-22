38ba2472ce5c0bf48dd63ac1f1db5bcdc8b70dfb
Richie <devtooligan@gmail.com>
2021-12-22 14:52:13 -0800

    dapp init YieldspaceVy

---
+++ src/YieldspaceVy.t.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity ^0.8.6;
+
+import "ds-test/test.sol";
+
+import "./YieldspaceVy.sol";
+
+contract YieldspaceVyTest is DSTest {
+    YieldspaceVy vy;
+
+    function setUp() public {
+        vy = new YieldspaceVy();
+    }
+
+    function testFail_basic_sanity() public {
+        assertTrue(false);
+    }
+
+    function test_basic_sanity() public {
+        assertTrue(true);
+    }
+}
